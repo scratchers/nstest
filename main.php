0f3949baa686e30ba44c2cb82f5aec78f26ee4c0
Jeff Puckett <jeff@jeffpuckett.com>
2016-10-11 22:35:57 -0500

    use fully qualified name

--- main.php
+++ main.php
@@ -1,6 +1,7 @@
 <?php
 require __DIR__.'/vendor/autoload.php';
 use scratchers\nstest\Container;
+use scratchers\nstest\MyClass;
 
-$obj = Container::get('MyClass');
+$obj = Container::get(MyClass::class);
 echo $obj->prop;
